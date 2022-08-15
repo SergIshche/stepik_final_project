a67f0d6286f1d9c31c3cbae2d9d2682dbf7f27c6
SERG <testsproba8@gmail.com>
2022-08-15 19:30:56 +0300

    login_page.py is added, locators.py is changed: class LoginPageLocators is added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
