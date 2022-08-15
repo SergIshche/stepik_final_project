4982872ae632c1d44d1fbcb8f3514ae61356a352
SERG <testsproba8@gmail.com>
2022-08-15 20:00:16 +0300

    The switch between two pages was implemented by modifying test_guest_can_go_to_login_page and go_to_login_page functions

--- login_page.py
+++ login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url
+        assert "login" in self.browser.current_url
 
 
     def should_be_login_form(self):
