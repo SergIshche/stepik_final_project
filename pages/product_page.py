f83e0fa7e98ea227956c68e814b6c17da44a084e
SERG <testsproba8@gmail.com>
2022-08-16 17:55:58 +0300

    Present/disappeared/login_page base_page: go_to_login_page, should_be_login_link, is_not_element_present,is_dissappeared functions are added; locators: class BasePageLocators is added; product_page.py: present/disappeared; test_product_page.py: 5 tests are added.

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,10 +10,22 @@ class ProductPage(BasePage):
 
     def check_that_the_item_added(self):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == \
-               self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED).text, "The message about added prodcut is wrong"
+               self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED).text, \
+            "The message about added prodcut is wrong"
 
     def check_that_the_price_is_correct(self):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text in \
-               self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_MESSAGE).text, "The price of added product is wrong"
+               self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_MESSAGE).text, \
+            "The price of added product is wrong"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def check_is_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
+
+
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import time
 
 
@@ -21,4 +22,39 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_that_the_item_added()
     page.check_that_the_price_is_correct()
+    page.should_not_be_success_message()
     #time.sleep(900)
+@pytest.mark.second_part
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_the_add_to_basket_button()
+    page.should_not_be_success_message()
+@pytest.mark.second_part
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+@pytest.mark.second_part
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_the_add_to_basket_button()
+    page.check_is_disappeared_success_message()
+@pytest.mark.login
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+@pytest.mark.login
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
