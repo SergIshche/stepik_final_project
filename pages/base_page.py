28e751e8c9f681aa6e4886a071c27469990b27b3
SERG <testsproba8@gmail.com>
2022-08-17 12:41:30 +0300

    Refactoring and design

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
-
 import math
 
 class BasePage():
